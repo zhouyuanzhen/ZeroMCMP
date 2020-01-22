704cd8f3b5b1053d3c22e15c2e600e4453c92d39
Zhou Yuanzhen <szrednick+zhouyuanzhen@gmail.com>
2020-01-22 14:05:04 +0800

    Add main python file

---
+++ zeromcmp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    print("************************************************************************")
+    print("*   WELCOME TO ZEROMCMP (OpenSource MultiCloud Management Platform)    *")
+    print("************************************************************************")
