c36b936d2bae6d778943c29a8b93966f67a334ac
Zhou Yuanzhen <szrednick+zhouyuanzhen@gmail.com>
2020-02-15 19:32:20 +0800

    Use Flask framework

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to ZeroMCMP!\n'
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=8080)
+
